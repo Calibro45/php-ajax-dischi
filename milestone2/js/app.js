9953c1e51463a3218c424b302cf7bfe038304e12
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-03-31 18:08:14 +0200

    chiamata generi

--- milestone2/js/app.js
+++ milestone2/js/app.js
@@ -4,6 +4,8 @@ const app = new Vue ({
 
     data: {
         albums: [],
+        generi: [],
+        genere: '',
     },
 
     methods: {
@@ -12,13 +14,57 @@ const app = new Vue ({
             axios.get('php/server.php')
             .then(resp => {
                 //console.log(resp.data);
+                
                 this.albums = resp.data;
+                this.getGeneri(resp.data);
             })
-        }
+        },
+        fetchGeneri: function() {
+
+
+            axios.get('php/server.php')
+            .then(resp => {
+
+                this.albums = [];
+                const dischi = resp.data;
+
+                if(this.genere != 'All') {
+
+                    dischi.forEach(el => {
+    
+                        const genre = el.genre;
+    
+                        if (genre === this.genere) {
+    
+                            this.albums.push(el);
+                        } 
+                    })
+                } else {
+
+                    this.albums = dischi;
+                }
+
+            })
+        },
+        getGeneri: function(el) {
+            el.forEach(element => {
+
+                const genere = element.genre;
+
+                if (!this.generi.includes(genere)) {
+
+                    this.generi.push(genere);
+                }
+            });
+        },
     },
     created() {
 
         this.fetchAlbum();
     },
+
+    // mounted() {
+    //     this.fetchGeneri();
+    // }
 });
 console.log(app);
