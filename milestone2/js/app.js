cfaae697eadd9178ddd6fd41a8bbde20f19d5fc8
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-03-31 15:56:28 +0200

    chiamata axios

--- milestone2/js/app.js
+++ milestone2/js/app.js
@@ -3,7 +3,22 @@ const app = new Vue ({
     el: '#app',
 
     data: {
-        message: 'ciao',
+        albums: [],
+    },
+
+    methods: {
+        fetchAlbum: function() {
+
+            axios.get('php/server.php')
+            .then(resp => {
+                //console.log(resp.data);
+                this.albums = resp.data;
+            })
+        }
+    },
+    created() {
+
+        this.fetchAlbum();
     },
 });
 console.log(app);
